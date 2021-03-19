52221769a5180cf2ae479597126dba609cfa9338
tuguzT <timurka.tugushev@gmail.com>
2021-03-19 15:14:21 +0300

    Layer and extension properties enumeration

--- src/graphics/instance.rs
+++ src/graphics/instance.rs
@@ -1,28 +1,39 @@
 use std::error::Error;
 
-use ash::{Entry, vk};
-use ash::version::{EntryV1_0, InstanceV1_0};
+use ash::version::EntryV1_0;
+use ash::vk;
 
 use crate::config::Config;
 use crate::version::Version;
 
 pub struct Instance {
-    vk_instance: vk::Instance,
-    vk_version: Version,
-    entry: Entry,
+    version: Version,
+    layer_properties: Vec<vk::LayerProperties>,
+    extension_properties: Vec<vk::ExtensionProperties>,
+    instance: ash::Instance,
+    lib_entry: ash::Entry,
 }
 
 impl Instance {
     pub fn new(config: &Config) -> Result<Self, Box<dyn Error>> {
-        let entry = unsafe {
-            Entry::new()?
+        let lib_entry = unsafe {
+            ash::Entry::new()?
         };
-        let application_version = to_vk_version(&config.app_version());
-        let engine_version = to_vk_version(&config.engine_version());
-        let vk_version = match entry.try_enumerate_instance_version() {
-            Ok(version) => from_vk_version(version.unwrap_or(0)),
+        let version = match lib_entry.try_enumerate_instance_version() {
+            Ok(version) => {
+                from_vk_version(version.unwrap_or(vk::API_VERSION_1_0))
+            },
             Err(_) => Version::default()
         };
+        let layer_properties = lib_entry
+            .enumerate_instance_layer_properties()
+            .unwrap_or(Vec::new());
+        let extension_properties = lib_entry
+            .enumerate_instance_extension_properties()
+            .unwrap_or(Vec::new());
+
+        let application_version = to_vk_version(&config.app_version());
+        let engine_version = to_vk_version(&config.engine_version());
         let application_info = vk::ApplicationInfo {
             p_application_name: config.app_name_c().as_ptr(),
             application_version,
@@ -31,32 +42,42 @@ impl Instance {
             api_version: vk::API_VERSION_1_2,
             ..Default::default()
         };
+        vk::ApplicationInfo::builder().build();
         let instance_create_info = vk::InstanceCreateInfo {
             p_application_info: &application_info,
             ..Default::default()
         };
-        let vk_instance: vk::Instance = unsafe {
-            entry.create_instance(&instance_create_info, None)?
-        }.handle();
-
-        println!("Instance was created! {:#?}", vk_version);
+        let instance = unsafe {
+            lib_entry.create_instance(&instance_create_info, None)?
+        };
+        println!("Instance was created! {:#?}", version);
         Ok(Self {
-            vk_instance,
-            vk_version,
-            entry,
+            instance,
+            version,
+            lib_entry,
+            layer_properties,
+            extension_properties,
         })
     }
 
-    fn vk_version(&self) -> &Version {
-        &self.vk_version
+    pub fn version(&self) -> &Version {
+        &self.version
     }
 
-    pub fn entry(&self) -> &Entry {
-        &self.entry
+    pub fn layer_properties(&self) -> &Vec<vk::LayerProperties> {
+        &self.layer_properties
     }
 
-    pub fn vk_instance(&self) -> vk::Instance {
-        self.vk_instance
+    pub fn instance(&self) -> &ash::Instance {
+        &self.instance
+    }
+
+    pub fn lib_entry(&self) -> &ash::Entry {
+        &self.lib_entry
+    }
+
+    pub fn extension_properties(&self) -> &Vec<vk::ExtensionProperties> {
+        &self.extension_properties
     }
 }
 
