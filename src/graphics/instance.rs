336a73c2187e8c52e45d712fe3e460b2b6a75147
tuguzT <timurka.tugushev@gmail.com>
2021-03-22 00:10:00 +0300

    to_vk_version and from_vk_version functions were moved into utils module, vectors of enabled devices and extensions are reserved for future

--- src/graphics/instance.rs
+++ src/graphics/instance.rs
@@ -6,15 +6,15 @@ use ash::vk;
 
 use crate::config::Config;
 use crate::version::Version;
+use crate::graphics::utils;
+use std::os::raw::c_char;
 
-const VALIDATION_LAYERS: [&str; 1] = [
-    "VK_LAYER_KHRONOS_validation",
-];
+const VALIDATION_LAYER_NAME: &str = "VK_LAYER_KHRONOS_validation";
 
 pub struct Instance {
     version: Version,
-    layer_properties: Vec<vk::LayerProperties>,
-    extension_properties: Vec<vk::ExtensionProperties>,
+    available_layer_properties: Vec<vk::LayerProperties>,
+    available_extension_properties: Vec<vk::ExtensionProperties>,
     instance: ash::Instance,
     lib_entry: ash::Entry,
 }
@@ -25,27 +25,37 @@ impl Instance {
             ash::Entry::new()?
         };
         let version = match lib_entry.try_enumerate_instance_version()? {
-            Some(version) => from_vk_version(version),
-            None => from_vk_version(vk::API_VERSION_1_0),
+            Some(version) => utils::from_vk_version(version),
+            None => utils::from_vk_version(vk::API_VERSION_1_0),
         };
-        let layer_properties = lib_entry
+        let available_layer_properties = lib_entry
             .enumerate_instance_layer_properties()?;
-        let extension_properties = lib_entry
+        let available_extension_properties = lib_entry
             .enumerate_instance_extension_properties()?;
 
         let application_name = CString::new(config.app_name())?;
         let engine_name = CString::new(config.engine_name())?;
         let application_info = vk::ApplicationInfo {
-            application_version: to_vk_version(&config.app_version()),
-            engine_version: to_vk_version(&config.engine_version()),
+            application_version: utils::to_vk_version(&config.app_version()),
+            engine_version: utils::to_vk_version(&config.engine_version()),
             p_application_name: application_name.as_ptr(),
             p_engine_name: engine_name.as_ptr(),
             api_version: vk::API_VERSION_1_2,
             ..Default::default()
         };
 
+        let mut enabled_layers_names: Vec<*const c_char> = Vec::new();
+        let c_validation_layer_name = CString::new(VALIDATION_LAYER_NAME)?;
+        if config.enable_validation() {
+            enabled_layers_names.push(c_validation_layer_name.as_ptr());
+        }
+        let enabled_extension_names: Vec<*const c_char> = Vec::new();
         let instance_create_info = vk::InstanceCreateInfo {
             p_application_info: &application_info,
+            enabled_layer_count: enabled_layers_names.len() as u32,
+            pp_enabled_layer_names: enabled_layers_names.as_ptr(),
+            enabled_extension_count: enabled_extension_names.len() as u32,
+            pp_enabled_extension_names: enabled_extension_names.as_ptr(),
             ..Default::default()
         };
         let instance = unsafe {
@@ -56,8 +66,8 @@ impl Instance {
             instance,
             version,
             lib_entry,
-            layer_properties,
-            extension_properties,
+            available_layer_properties,
+            available_extension_properties,
         })
     }
 
@@ -65,12 +75,12 @@ impl Instance {
         &self.version
     }
 
-    pub fn layer_properties(&self) -> &Vec<vk::LayerProperties> {
-        &self.layer_properties
+    pub fn available_layer_properties(&self) -> &Vec<vk::LayerProperties> {
+        &self.available_layer_properties
     }
 
-    pub fn extension_properties(&self) -> &Vec<vk::ExtensionProperties> {
-        &self.extension_properties
+    pub fn available_extension_properties(&self) -> &Vec<vk::ExtensionProperties> {
+        &self.available_extension_properties
     }
 
     pub fn instance(&self) -> &ash::Instance {
@@ -88,22 +98,4 @@ impl Drop for Instance {
             self.instance.destroy_instance(None);
         }
     }
-}
-
-#[inline]
-fn to_vk_version(version: &Version) -> u32 {
-    vk::make_version(
-        version.major,
-        version.minor,
-        version.patch,
-    )
-}
-
-#[inline]
-fn from_vk_version(version: u32) -> Version {
-    Version::new(
-        vk::version_major(version),
-        vk::version_minor(version),
-        vk::version_patch(version),
-    )
 }
--- src/graphics/utils.rs
+++ src/graphics/utils.rs
@@ -1,3 +1,7 @@
+use ash::vk;
+
+use crate::version::Version;
+
 #[macro_export]
 macro_rules! c_str {
     ($s:expr) => {
@@ -5,4 +9,22 @@ macro_rules! c_str {
             std::ffi::CStr::from_ptr(concat!($s, "\0").as_ptr() as *const std::os::raw::c_char)
         }
     };
+}
+
+#[inline]
+pub fn to_vk_version(version: &Version) -> u32 {
+    vk::make_version(
+        version.major,
+        version.minor,
+        version.patch,
+    )
+}
+
+#[inline]
+pub fn from_vk_version(version: u32) -> Version {
+    Version::new(
+        vk::version_major(version),
+        vk::version_minor(version),
+        vk::version_patch(version),
+    )
 }
