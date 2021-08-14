29f704bd80545cc2d5bd953023abd7fddd0112f4
tuguzT <timurka.tugushev@gmail.com>
2021-08-14 17:57:23 +0300

    Refactor pattern matching in callback

--- titan-engine/src/graphics/debug_callback.rs
+++ titan-engine/src/graphics/debug_callback.rs
@@ -3,16 +3,16 @@ use vulkano::instance::debug::{Message, MessageSeverity, MessageType};
 
 pub fn callback(message: &Message) {
     let level = match message.severity {
-        MessageSeverity { verbose, .. } if verbose => Level::Trace,
-        MessageSeverity { information, .. } if information => Level::Info,
-        MessageSeverity { warning, .. } if warning => Level::Warn,
-        MessageSeverity { error, .. } if error => Level::Error,
-        _ => Level::Debug,
+        MessageSeverity { verbose: true, .. } => Level::Trace,
+        MessageSeverity { information: true, .. } => Level::Info,
+        MessageSeverity { warning: true, .. } => Level::Warn,
+        MessageSeverity { error: true, .. } => Level::Error,
+        _ => Level::Trace,
     };
     let ty = match message.ty {
-        MessageType { general, .. } if general => "GENERAL",
-        MessageType { validation, .. } if validation => "VALIDATION",
-        MessageType { performance, .. } if performance => "PERFORMANCE",
+        MessageType { general: true, .. } => "GENERAL",
+        MessageType { validation: true, .. } => "VALIDATION",
+        MessageType { performance: true, .. } => "PERFORMANCE",
         _ => "NONE",
     };
     log::log!(
